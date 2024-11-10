33eeeb9cda4ec40f601d1639153148c408bda1dc
Hashen Udara <106765093+HashenUdara@users.noreply.github.com>
2024-11-10 13:02:40 +0530

    update features section

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -140,22 +140,18 @@ const HomePage = () => {
         <div className="hidden md:block absolute top-0 -right-1/3 w-72 h-72 bg-primary rounded-full blur-[10rem] -z-10"></div>
         <div className="hidden md:block absolute bottom-0 -left-1/3 w-72 h-72 bg-indigo-600 rounded-full blur-[10rem] -z-10"></div>
         <Container>
-          <div className="max-w-md mx-auto text-start md:text-center">
+          <div className="max-w-2xl mx-auto text-start md:text-center">
             <SectionBadge title="Features" />
             <h2 className="text-3xl lg:text-4xl font-semibold mt-6">
-              Discover our powerful features
+              Powerful Features for a Seamless Workshop Experience
             </h2>
             <p className="text-muted-foreground mt-6">
-              Astra offers a range of features to help you build a stunning
-              website in no time
+              Unlock the full potential of your event with tools designed to
+              engage, organize, and enhance every session.
             </p>
           </div>
         </Container>
-        <Container>
-          <div className="flex items-center justify-center mx-auto mt-8">
-            <Icons.feature className="w-auto h-80" />
-          </div>
-        </Container>
+
         <Container>
           <div className="flex flex-col items-center justify-center py-10 md:py-20 w-full">
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full gap-8">
@@ -174,68 +170,6 @@ const HomePage = () => {
                 </div>
               ))}
             </div>
-          </div>
-        </Container>
-      </Wrapper>
-
-      {/* pricing */}
-      <Wrapper className="flex flex-col items-center justify-center py-12 relative">
-        <div className="hidden md:block absolute top-0 -right-1/3 w-72 h-72 bg-blue-500 rounded-full blur-[10rem] -z-10"></div>
-        <Container>
-          <div className="max-w-md mx-auto text-start md:text-center">
-            <SectionBadge title="Pricing" />
-            <h2 className="text-3xl lg:text-4xl font-semibold mt-6">
-              Unlock the right plan for your business
-            </h2>
-            <p className="text-muted-foreground mt-6">
-              Choose the best plan for your business and start building your
-              dream website today
-            </p>
-          </div>
-        </Container>
-        <Container className="flex items-center justify-center">
-          <div className="grid grid-cols-1 lg:grid-cols-3 gap-5 w-full md:gap-8 py-10 md:py-20 flex-wrap max-w-4xl">
-            {pricingCards.map((card) => (
-              <Card
-                key={card.title}
-                className={cn(
-                  "flex flex-col w-full border-neutral-700",
-                  card.title === "Unlimited Saas" && "border-2 border-primary"
-                )}
-              >
-                <CardHeader className="border-b border-border">
-                  <span>{card.title}</span>
-                  <CardTitle
-                    className={cn(
-                      card.title !== "Unlimited Saas" && "text-muted-foreground"
-                    )}
-                  >
-                    {card.price}
-                  </CardTitle>
-                  <CardDescription>{card.description}</CardDescription>
-                </CardHeader>
-                <CardContent className="pt-6 space-y-3">
-                  {card.features.map((feature) => (
-                    <div key={feature} className="flex items-center gap-2">
-                      <Zap className="w-4 h-4 fill-primary text-primary" />
-                      <p>{feature}</p>
-                    </div>
-                  ))}
-                </CardContent>
-                <CardFooter className="mt-auto">
-                  <Link
-                    href="#"
-                    className={cn(
-                      "w-full text-center text-primary-foreground bg-primary p-2 rounded-md text-sm font-medium",
-                      card.title !== "Unlimited Saas" &&
-                        "!bg-foreground !text-background"
-                    )}
-                  >
-                    {card.buttonText}
-                  </Link>
-                </CardFooter>
-              </Card>
-            ))}
           </div>
         </Container>
       </Wrapper>
